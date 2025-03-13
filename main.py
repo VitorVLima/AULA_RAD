0ff016c9697aea8b264492f06a90a6de175c10d7
VITOR VINICIUS LIMA SILVA <202402966031@alunos.estacio.br>
2025-03-13 20:08:57 -0300

    atual

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import sqlite3
 banco = sqlite3.connect('database.db')
 cursor = banco.cursor() #para escrever em sql, inserir e manipular dados no banco
 
+#cursor.execute("CREATE TABLE cliente(nome text, idade integer, sexo text)")
+cursor.execute("INSERT INTO cliente VALUES ('vitor',26,'m'), ('paula',24,'f')")
+
+banco.commit()
+
+cursor.execute("SELECT * from cliente")
+print(cursor.fetchall())
+
+cursor.close()
+banco.close()
